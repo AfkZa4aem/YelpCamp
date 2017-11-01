9f54e7eddbd6f5e3a94fcd216c91e13a0d6eb573
Maxim <nastypinagrably@gmail.com>
2017-11-01 06:17:23 +0000

    update db info

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes 		= require("./routes/comments"),
 		indexRoutes			 	= require("./routes/index");
 
 var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
-mongoose.connect(process.env.DATABASEURL);
+mongoose.connect(url);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
